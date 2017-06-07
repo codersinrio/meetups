153040d61f7d9a29f1407e3cb131e8e33939ccde
Rafael Cruz <rafaelcruz_net@yahoo.com.br>
2017-06-06 23:48:58 -0300

    Adicionando o segundo meetup

---
+++ 2-Meetup/BDD+SpecFlow+Selenium/BDD/BehaviorDriveDevelopment/BDD/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BDD.MvcApplication" Language="C#" %>
---
+++ 2-Meetup/BDD+SpecFlow+Selenium/BDD/BehaviorDriveDevelopment/BDD/Models/UserModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BDD.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage="Nome é obrigatório")]
+        public String Nome { get; set; }
+
+        [Required(ErrorMessage="Email é obrigatório")]
+        [EmailAddress(ErrorMessage="Email é obrigatório")]
+        public String Email { get; set; }
+
+        [Required(ErrorMessage= "Password é obrigatório")]
+        public String Password { get; set; }
+    }
+}
