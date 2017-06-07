20cacc721dd33f9d2b489d1f6f1515d6bd17a6d9
Rafael Cruz <rafaelcruz_net@yahoo.com.br>
2017-06-07 00:15:03 -0300

    Palestras do segundo encontro

---
+++ 2-Meetup/BDD-SpecFlow-Selenium/BDD/BDD/BDD/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using BDD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BDD.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(UserModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            return Redirect("~/Home");
+        }
+
+    }
+}
